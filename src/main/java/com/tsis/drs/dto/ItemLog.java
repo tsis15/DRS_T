6db4026a57841b78210aee6c53a7e71d461ae1df
KimJaeHyun94 <ru6300@naver.com>
2022-03-09 19:24:00 +0900

    add itemlog

---
+++ src/main/java/com/tsis/drs/dto/ItemLog.java
@@ -0,0 +1,17 @@
+package com.tsis.drs.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class ItemLog {
+    String name; // 대여자
+    Date rental_date; // 대여날짜
+    String checker_name; // 반납확인자
+    Date return_date; // 반납일자
+}
