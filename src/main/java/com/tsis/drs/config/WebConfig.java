e45de66255e1a7925b7a6c352d93d0c9aea1cf05
Lee <dlwnsdud3737@naver.com>
2022-03-11 02:49:58 +0900

    add DocumentController selectOne

--- src/main/java/com/tsis/drs/config/WebConfig.java
+++ src/main/java/com/tsis/drs/config/WebConfig.java
@@ -1,13 +1,16 @@
 package com.tsis.drs.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
