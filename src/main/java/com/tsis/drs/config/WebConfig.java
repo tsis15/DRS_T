fc4fe381a8308bb281fac594f6441a67a3fd32ab
Lee <dlwnsdud3737@naver.com>
2022-03-11 02:56:03 +0900

    modify webconfig

--- src/main/java/com/tsis/drs/config/WebConfig.java
+++ src/main/java/com/tsis/drs/config/WebConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:8080", "http://localhost:8081")
-                .allowCredentials(true)
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
